2c8922354ffdb0a1bf2f8e84729e6a55e59251f1
Odilon Enrique <odilonegduarte@gmail.com>
2023-11-21 20:54:55 -0300

    add script newGame

---
+++ scripts/newGame.js
@@ -0,0 +1,25 @@
+const inputName = document.querySelector(".inputName");
+const newGameButton = document.querySelector(".newGameButton");
+const newGameForm = document.querySelector(".newGameForm");
+
+function validateInput(event) {
+    // console.log(event.target.value.length);
+    if (event.target.value.length >= 3) {
+        // remove disable
+        newGameButton.removeAttribute("disabled");
+    } else {
+        newGameButton.setAttribute("disabled", "true"); //Por default o valor de disabled é true(verdadeiro)
+    }
+}
+
+function handleSubmitNewGame(event) {
+    event.preventDefault();
+    console.log(inputName.value);
+    localStorage.setItem("@memmoryGame:player_name", inputName.value);
+    inputName.value = "";
+    window.location.href = "pages/cards.html";
+}
+
+inputName.addEventListener("input", validateInput);
+newGameForm.addEventListener("submit", handleSubmitNewGame);
+
