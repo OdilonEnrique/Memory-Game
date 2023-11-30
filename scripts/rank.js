630fc9d56095c68bc14988806767a25b7b7d0e4d
Odilon Enrique <odilonegduarte@gmail.com>
2023-11-30 20:26:15 -0300

    add function createBodyTableRank

--- scripts/rank.js
+++ scripts/rank.js
@@ -1,7 +1,38 @@
 function backPage() {
-      window.history.back();
+  window.history.back();
+}
+
+function createBodyTableRank() {
+  const storageRank = JSON.parse(localStorage.getItem("@memmoryGame:rank"));
+
+  let rankSorted;
+
+  if (storageRank) {
+    rankSorted = storageRank
+      .sort((a, b) => {
+        if (a.time > b.time) return 1;
+        if (b.time > a.time) return -1;
+        return 0;
+      })
+      .filter((value, index) => index < 10);
   }
 
+  bodyTableRank.innerHTML = "";
+
+  for (let i = 0; i < 10; i++) {
+    bodyTableRank.innerHTML += `
+      <tr>
+        <td>${i + 1}</td>
+        <td>${rankSorted ? rankSorted[i]?.name || "" : ""}</td>
+        <td>${rankSorted ? rankSorted[i]?.time || "" : ""}</td>  
+      </tr>
+    `;
+  }
+}
+
 const backButton = document.querySelector(".backButton");
+const bodyTableRank = document.querySelector(".tableRank tbody");
 
 backButton.addEventListener("click", backPage);
+
+createBodyTableRank();
